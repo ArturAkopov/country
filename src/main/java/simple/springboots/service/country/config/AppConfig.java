c168e06afe83d0384fa6f2f06032469b7226ddef
Artur <116442638+ArturAkopov@users.noreply.github.com>
2025-04-09 00:53:13 +0300

    Добавлена реализация через SOUP

--- src/main/java/simple/springboots/service/country/config/AppConfig.java
+++ src/main/java/simple/springboots/service/country/config/AppConfig.java
@@ -1,18 +1,56 @@
 package simple.springboots.service.country.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 import java.text.SimpleDateFormat;
 
 @Configuration
+@EnableWs
 public class AppConfig {
+
+    public static final String SOUP_NAMESPACE = "country";
+
+    @Value("${country.base-uri}")
+    private String countryBaseUri;
 
     @Bean
     public ObjectMapper objectMapper(){
         ObjectMapper om = new ObjectMapper();
         om.setDateFormat(new SimpleDateFormat("dd.MM.yyyy"));
         return om;
+    }
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "country")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countrySchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CountryPort");
+        wsdl11Definition.setLocationUri(countryBaseUri + "/ws");
+        wsdl11Definition.setTargetNamespace(SOUP_NAMESPACE);
+        wsdl11Definition.setSchema(countrySchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countrySchema() {
+        return new SimpleXsdSchema(new ClassPathResource("country.xsd"));
     }
 }
---
+++ src/main/java/simple/springboots/service/country/service/soup/CountryEndpoint.java
@@ -0,0 +1,87 @@
+package simple.springboots.service.country.service.soup;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import simple.springboots.service.country.config.AppConfig;
+import simple.springboots.service.country.domain.graphql.CountryGql;
+import simple.springboots.service.country.domain.graphql.CountryInputGql;
+import simple.springboots.service.country.service.CountryService;
+import xml.country.*;
+
+
+@Endpoint
+public class CountryEndpoint {
+
+    private final CountryService countryService;
+
+    @Autowired
+    public CountryEndpoint(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @PayloadRoot(namespace = AppConfig.SOUP_NAMESPACE, localPart = "AddCountryInputRequest")
+    @ResponsePayload
+    public CountryResponse addCountry(@RequestPayload AddCountryInputRequest request) {
+        final CountryInputGql inputGql = new CountryInputGql(
+                request.getCountry().getCountryName(),
+                request.getCountry().getCountryCode());
+
+        final CountryGql country = countryService.addGqlCountry(inputGql);
+
+        Country xmlCountry = new Country();
+        xmlCountry.setId(country.id().toString());
+        xmlCountry.setCountryName(country.countryName());
+        xmlCountry.setCountryCode(country.countryCode());
+
+        CountryResponse response = new CountryResponse();
+        response.setCountry(xmlCountry);
+        return response;
+    }
+
+    @PayloadRoot(namespace = AppConfig.SOUP_NAMESPACE, localPart = "EditCountryInputRequest")
+    @ResponsePayload
+    public CountryResponse editCountry(@RequestPayload EditCountryInputRequest request) {
+        final CountryInputGql inputGql = new CountryInputGql(
+                request.getCountry().getCountryName(),
+                request.getCountry().getCountryCode());
+
+        final CountryGql country = countryService.editGqlCountryName(inputGql);
+
+        Country xmlCountry = new Country();
+        xmlCountry.setId(country.id().toString());
+        xmlCountry.setCountryName(country.countryName());
+        xmlCountry.setCountryCode(country.countryCode());
+
+        CountryResponse response = new CountryResponse();
+        response.setCountry(xmlCountry);
+        return response;
+    }
+
+    @PayloadRoot(namespace = AppConfig.SOUP_NAMESPACE, localPart = "PageRequest")
+    @ResponsePayload
+    public CountriesResponse allCountries(@RequestPayload PageRequest request) {
+        Page<CountryGql> countries = (Page<CountryGql>) countryService.allGqlCountries(
+                org.springframework.data.domain.PageRequest.of(
+                request.getPage(),
+                request.getSize()
+        ));
+        CountriesResponse response = new CountriesResponse();
+        response.setTotalPages(countries.getTotalPages());
+        response.setTotalElements(countries.getTotalElements());
+        response.getCountries().addAll(
+                countries.getContent().stream().map(
+                        countryGql -> {
+                            Country xmlCountry = new Country();
+                            xmlCountry.setId(countryGql.id().toString());
+                            xmlCountry.setCountryName(countryGql.countryName());
+                            xmlCountry.setCountryCode(countryGql.countryCode());
+                            return xmlCountry;
+                        }
+                ).toList());
+        return response;
+    }
+}
